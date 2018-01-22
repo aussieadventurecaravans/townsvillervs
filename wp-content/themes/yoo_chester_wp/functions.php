b168f978b75f9de2fc18ecbde210cfeb14136436
sonnguyen89 <nam_son14@yahoo.com>
2018-01-22 12:30:08 +1100

    hardcore --- hide the 'Accessories & parts' links at other categories except others pages

--- wp-content/themes/yoo_chester_wp/functions.php
+++ wp-content/themes/yoo_chester_wp/functions.php
@@ -68,3 +68,24 @@ function woocommerce_product_contact_form7_tab(){
 }
 
 add_filter( 'loop_shop_per_page', create_function( '$cols', 'return 48;' ), 20 );
+
+
+//customize the product categories at sidebar
+
+add_filter('woocommerce_product_categories_widget_args','customize_product_categories',99,1);
+function customize_product_categories($list_args)
+{
+    //hard code --- hide the category link show up at some category page except this category page
+    $hide_category_id = '67';
+    if($list_args['current_category'] != $hide_category_id || $list_args['current_category_ancestors'][0] != $hide_category_id)
+    {
+        $category_list = $list_args['include'] ;
+        //remove the category from the list
+        $list_args['include'] =  str_replace($hide_category_id,'',$category_list);
+        
+    }
+
+
+    //echo print_r($list_args,true); die;
+    return $list_args;
+}
